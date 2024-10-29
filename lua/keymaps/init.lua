051ffa5c0e5980c0a07656111bdb7281c1118a46
NaruseNia <notshinsin0000@gmail.com>
2024-10-29 14:40:58 +0900

    haha...

--- lua/keymaps/init.lua
+++ lua/keymaps/init.lua
@@ -1,6 +1,7 @@
 local builtin = require("telescope.builtin")
 local hop = require("hop")
 local dircs = require("hop.hint").HintDirection
+local crates = require("crates")
 
 vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
@@ -16,11 +17,16 @@ vim.keymap.set("n", "<leader>fz", builtin.lsp_workspace_symbols, { desc = "Teles
 vim.keymap.set("n", "<leader>vc", builtin.git_commits, { desc = "Telescope LSP commits" })
 vim.keymap.set("n", "<leader>vs", builtin.git_status, { desc = "Telescope git status" })
 vim.keymap.set("n", "<leader>vb", builtin.git_branches, { desc = "Telescope git branches" })
+vim.keymap.set("n", "<leader>ct", crates.toggle, { desc = "Toggle crates ui" })
+vim.keymap.set("n", "<leader>cc", crates.show_popup, { desc = "Show crates popup" })
+vim.keymap.set("n", "<leader>cf", crates.show_features_popup, { desc = "Show crates features" })
+vim.keymap.set("n", "<leader>cd", crates.open_documentation, { desc = "Open crates' documentation" })
+vim.keymap.set("n", "<leader>ci", crates.open_crates_io, { desc = "Open crates' page" })
 
 vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { desc = "Toggle NeoTree" })
 
 vim.keymap.set("n", "<A-;>", function()
-	vim.lsp.buf.format({ async = false })
+  vim.lsp.buf.format({ async = false })
 end, { desc = "Format document" })
 
 vim.keymap.set("n", "<C-x>", ":BufferClose<CR>", { desc = "Format document" })
@@ -36,23 +42,23 @@ vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 vim.keymap.set("t", "<leader>cr", ":so $MYVIMRC<CR>", { desc = "Reload config" })
 
 vim.keymap.set("", "<Leader>r", function()
-	hop.hint_char1({ direction = dircs.AFTER_CURSOR, currend_line_only = false })
+  hop.hint_char1({ direction = dircs.AFTER_CURSOR, currend_line_only = false })
 end, { remap = true })
 vim.keymap.set("", "<Leader>R", function()
-	hop.hint_char1({ direction = dircs.BEFORE_CURSOR, currend_line_only = false })
+  hop.hint_char1({ direction = dircs.BEFORE_CURSOR, currend_line_only = false })
 end, { remap = true })
 vim.keymap.set("", "<Leader>z", function()
-	hop.hint_char2({ direction = dircs.AFTER_CURSOR, currend_line_only = false })
+  hop.hint_char2({ direction = dircs.AFTER_CURSOR, currend_line_only = false })
 end, { remap = true })
 vim.keymap.set("", "<Leader>Z", function()
-	hop.hint_char2({ direction = dircs.BEFORE_CURSOR, currend_line_only = false })
+  hop.hint_char2({ direction = dircs.BEFORE_CURSOR, currend_line_only = false })
 end, { remap = true })
 
 vim.keymap.set(
-	"i",
-	"<C-z>",
-	'copilot#Accept("<CR>")',
-	{ silent = true, expr = true, replace_keycodes = false, script = true }
+  "i",
+  "<C-z>",
+  'copilot#Accept("<CR>")',
+  { silent = true, expr = true, replace_keycodes = false, script = true }
 )
 
 vim.keymap.set("n", "<C-i>", "mmyyp`mj", { desc = "Duplicate line", noremap = true, silent = true })
