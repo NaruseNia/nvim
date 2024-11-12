d1a88b6b1f8e7e150cc61bff498beee6e3a295d7
NaruseNia <notshinsin0000@gmail.com>
2024-11-13 02:02:28 +0900

    Add gradle.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -58,6 +58,24 @@ require("lazy").setup({
 			require("crates").setup()
 		end,
 	},
+	{
+		"oclay1st/gradle.nvim",
+		cmd = { "Gradle", "GradleExec", "GradleInit" },
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+		},
+		opts = {}, -- options, see default configuration
+		keys = {
+			{
+				"<Leader>G",
+				function()
+					require("gradle").toggle_projects_view()
+				end,
+				desc = "Gradle",
+			},
+		},
+	},
 
 	-- Treesitter
 	{
