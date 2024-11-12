fefa590a3f1bf8548b4cb48ba53937ef948d8a38
NaruseNia <notshinsin0000@gmail.com>
2024-11-13 02:10:06 +0900

    hidden lsp diag

--- init.lua
+++ init.lua
@@ -38,4 +38,12 @@ vim.opt.guifont = "ZedMono Nerd Font"
 vim.cmd("language en_US")
 -- vim.cmd("colorscheme obscure")
 
+vim.diagnostic.config({
+	virtual_text = false,
+})
+
+-- Show line diagnostics automatically in hover window
+vim.o.updatetime = 250
+vim.cmd([[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]])
 require("autocmd")
+
