4075cb0169fc3f855170492639b9e74442b0e489
NaruseNia <notshinsin0000@gmail.com>
2025-04-08 09:03:55 +0900

    avante

--- init.lua
+++ init.lua
@@ -33,9 +33,7 @@ vim.opt.cursorline = true
 
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.guifont = "ZedMono Nerd Font"
 
-vim.cmd("language en_US")
 -- vim.cmd("colorscheme obscure")
 
 vim.diagnostic.config({
--- lua/plugins/config/toggleterm.lua
+++ lua/plugins/config/toggleterm.lua
@@ -1,6 +1,2 @@
-if vim.fn.has("win32") or vim.fn.has("win64") then
 	require("toggleterm").setup({
-		-- For windows
-		shell = "pwsh",
 	})
-end
