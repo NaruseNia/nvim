8b8eae1479a82074b59dd1393c17ec0988d23464
NaruseNia <notshinsin0000@gmail.com>
2025-07-20 11:57:02 +0900

    windows java

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,12 +1,22 @@
+local os_util = require("libs.os_util")
 local jdtls_setup = require("jdtls.setup")
+
 local root_markers = { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }
 local root_dir = jdtls_setup.find_root(root_markers)
 if not root_dir then
   return
 end
 
+local get_jdtls = function(home_dir)
+  if os_util.is_windows_native() then
+    return home_dir .. "/AppData/Local/nvim-data/mason/packages/jdtls"
+  else
+    return home_dir .. "/.local/share/nvim/mason/packages/jdtls"
+  end
+end
+
 local home = os.getenv("HOME")
-local jdtls_path = home .. "/.local/share/nvim/mason/packages/jdtls"
+local jdtls_path = get_jdtls(home)
 local config_dir = jdtls_path .. "/config_mac" -- if you use linux /config_linux
 local launcher_path = vim.fn.glob(jdtls_path .. "/plugins/org.eclipse.equinox.launcher_*.jar")
 local project_name = vim.fn.fnamemodify(root_dir, ":p:h:t")
