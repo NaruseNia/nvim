17b3dfad8328e1897bd228bce174febffc3f1d1d
NaruseNia <notshinsin0000@gmail.com>
2025-07-23 16:24:15 +0900

    fix detect os

--- lua/libs/os_util.lua
+++ lua/libs/os_util.lua
@@ -1,11 +1,11 @@
 local exports = {}
 
 function exports.is_windows_native()
-  return vim.fn.has("windows") == 1 and vim.fn.has("wsl") ~= 1
+  return (vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1) and vim.fn.has("wsl") ~= 1
 end
 
 function exports.is_windows()
-  return vim.fn.has("windows")
+  return vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1
 end
 
 function exports.is_wsl()
