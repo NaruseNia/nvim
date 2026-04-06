177e177e13ff80ffde00b6d602a1386d67889ac1
Naruse Nia <notshinsin0000@gmail.com>
2026-04-06 13:34:24 +0900

    chore: disable copilot autocmd

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -29,16 +29,16 @@ vim.api.nvim_create_autocmd("LspAttach", {
 })
 
 -- Copilot suggetion
-vim.api.nvim_create_autocmd("User", {
-  pattern = "BlinkCmpMenuOpen",
-  callback = function()
-    vim.b.copilot_suggestion_hidden = true
-  end,
-})
-
-vim.api.nvim_create_autocmd("User", {
-  pattern = "BlinkCmpMenuClose",
-  callback = function()
-    vim.b.copilot_suggestion_hidden = false
-  end,
-})
+-- vim.api.nvim_create_autocmd("User", {
+--   pattern = "BlinkCmpMenuOpen",
+--   callback = function()
+--     vim.b.copilot_suggestion_hidden = true
+--   end,
+-- })
+--
+-- vim.api.nvim_create_autocmd("User", {
+--   pattern = "BlinkCmpMenuClose",
+--   callback = function()
+--     vim.b.copilot_suggestion_hidden = false
+--   end,
+-- })
